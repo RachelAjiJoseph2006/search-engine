2b14fdc6aa42a5b16cd665acfc61781f849c6bcb
RachelAjiJoseph2006 <rachelalternativespace@gmail.com>
2026-06-22 20:53:11 +0530

    Remove old Azure workflows

--- startup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Starting Gunicorn server from startup.sh script..."
-gunicorn --bind=0.0.0.0 --timeout 600 --workers 4 --worker-class uvicorn.workers.UvicornWorker --chdir backend main:app
